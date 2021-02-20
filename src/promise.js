25082aed6c1616985fe3479d298ae00f81d02513
mzyaimll <2592972482@qq.com>
2021-02-20 11:17:11 +0800

    update project

--- src/promise.js
+++ src/promise.js
@@ -2,18 +2,26 @@
  * @Autor: GeekMzy
  * @Date: 2021-02-20 06:59:06
  * @LastEditors: GeekMzy
- * @LastEditTime: 2021-02-20 07:00:33
+ * @LastEditTime: 2021-02-20 07:02:13
  * @FilePath: /js-base/src/promise.js
  */
 
 const promise = new Promise((resolve, reject) => {
   console.log(1);
-  resolve()
+  resolve(5);
   console.log(2);
-})
+}).then(val => {
+  console.log(val);
+});
 
 promise.then(() => {
   console.log(3);
-})
+});
 
 console.log(4);
+
+setTimeout(function () {
+  console.log(6);
+});
+
+//执行结果: 124536
